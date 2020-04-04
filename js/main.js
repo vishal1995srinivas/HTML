b490b7e58e68ddef20eaededf5b8b48fe44de901
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-04 18:39:27 +0530

    site contetent started

--- js/main.js
+++ js/main.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
 	$nav = $('.nav');
 	$toggleCollapse = $('.toggle-collapse');
 
-	/*Click event on toggle menu*/
+	/** click event on toggle menu */
 	$toggleCollapse.click(function() {
 		$nav.toggleClass('collapse');
 	});
 
-	//Owl carousel for blog
-	$('owl-carousel').owlCarousel({
+	// owl-crousel for blog
+	$('.owl-carousel').owlCarousel({
 		loop: true,
-		autoplay: true,
+		autoplay: false,
 		autoplayTimeout: 3000,
 		dots: false,
 		nav: true,
-		navText: [ $('.owl-navigation .owl-nav-prev'), $('.owl-nav-next') ]
+		navText: [ $('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next') ],
+		responsive: responsive
 	});
 });
