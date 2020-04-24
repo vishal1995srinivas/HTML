7b2d29f78131e3a68aba40bd8e23a2971cf919d1
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-24 19:38:43 +0530

    Content and css added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+	0: {
+		items: 1
+	},
+	320: {
+		items: 1
+	},
+	560: {
+		items: 2
+	},
+	960: {
+		items: 3
+	}
+};
+
 $(document).ready(function() {
 	$nav = $('.nav');
 	$toggleCollapse = $('.toggle-collapse');
@@ -17,4 +32,17 @@ $(document).ready(function() {
 		navText: [ $('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next') ],
 		responsive: responsive
 	});
+
+	// click to scroll top
+	$('.move-up span').click(function() {
+		$('html, body').animate(
+			{
+				scrollTop: 0
+			},
+			1000
+		);
+	});
+
+	// AOS Instance
+	AOS.init();
 });
